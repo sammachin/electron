3ab911f41e42661e7793a3831501e8dbc06f10fb
John Kleinschmidt <jkleinsc@github.com>
2018-03-21 14:03:18 -0400

    Try creating release

--- script/test.js
+++ script/test.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+require('colors')
 const assert = require('assert')
 const GitHub = require('github')
+const { GitProcess } = require('dugite')
+const fail = '\u2717'.red
+const pass = '\u2713'.green
+const path = require('path')
+const gitDir = path.resolve(__dirname, '..')
 
 assert(process.env.ELECTRON_GITHUB_TOKEN, 'ELECTRON_GITHUB_TOKEN not found in environment')
 
@@ -8,18 +14,58 @@ const github = new GitHub({
   followRedirects: false
 })
 github.authenticate({type: 'token', token: process.env.ELECTRON_GITHUB_TOKEN})
-const githubOpts = {
-  owner: 'electron',
-  repo: 'electron'
-}
 
 async function getReleases () {
+  let newVersion = '2.0.0-beta.6'
+  let branchToTarget = await getCurrentBranch(gitDir)
   console.log(`About to get releases.`)
+  const githubOpts = {
+    owner: 'electron',
+    repo: 'electron'
+  }
   let releases = await github.repos.getReleases(githubOpts)
     .catch(err => {
       console.log('$fail} Could not get releases.  Error was', err)
     })
   console.log(`Releases are`, releases)
+
+  let drafts = releases.data.filter(release => release.draft &&
+    release.tag_name === newVersion)
+  if (drafts.length > 0) {
+    console.log(`${fail} Aborting because draft release for
+      ${drafts[0].tag_name} already exists.`)
+    process.exit(1)
+  }
+  console.log(`${pass} A draft release does not exist; creating one.`)
+  githubOpts.draft = true
+  githubOpts.name = `electron ${newVersion}`
+  githubOpts.body = 'This is just a test -- ignore me'
+  githubOpts.tag_name = newVersion
+  githubOpts.target_commitish = branchToTarget
+  console.log(`Trying to createRelease for ${newVersion}`, githubOpts)
+  await github.repos.createRelease(githubOpts)
+    .catch(err => {
+      console.log(`${fail} Error creating new release: `, err)
+      process.exit(1)
+    })
+  console.log(`${pass} Draft release for ${newVersion} has been created.`)
+}
+
+async function getCurrentBranch () {
+  console.log(`Determining current git branch`)
+  let gitArgs = ['rev-parse', '--abbrev-ref', 'HEAD']
+  let branchDetails = await GitProcess.exec(gitArgs, gitDir)
+  if (branchDetails.exitCode === 0) {
+    let currentBranch = branchDetails.stdout.trim()
+    console.log(`${pass} Successfully determined current git branch is ` +
+      `${currentBranch}`)
+    return currentBranch
+  } else {
+    let error = GitProcess.parseError(branchDetails.stderr)
+    console.log(`${fail} Could not get details for the current branch,
+      error was ${branchDetails.stderr}`, error)
+    process.exit(1)
+  }
 }
 
 getReleases()
